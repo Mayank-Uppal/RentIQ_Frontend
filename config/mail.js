97807deea47ff35832d68a5c059e761ea4beead5
Mayank <mayank@Mayanks-MacBook-Air.local>
2026-03-23 22:31:18 +0530

    mail.js changes

--- config/mail.js
+++ config/mail.js
@@ -1,4 +1,3 @@
-import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -13,7 +12,7 @@ const resend = new Resend(process.env.resend_api_key); */
   html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
 });  */
 
-const transporter = nodemailer.createTransport({
+/* const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",       
     port: 587,                      
     secure:false,
@@ -22,7 +21,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.gmail_user,
         pass: process.env.gmail_pass
     }
-})
+}) */
 
 const sendMail = async (email, token) => {
     try {
@@ -30,24 +29,49 @@ const sendMail = async (email, token) => {
         const invite = `http://localhost:5173/invite?token=${token}`;
         const message = `<p>Dear Tenant,</p>
 
-<p>You have been invited to join a rental property on RentIQ.</p>
+        <p>You have been invited to join a rental property on RentIQ.</p>
 
-<p>Please click the link below to accept the invitation:</p>
+        <p>Please click the link below to accept the invitation:</p>
 
-<p> <a href="${invite}">Accept Invite</a></p>
+        <p> <a href="${invite}">Accept Invite</a></p>
 
-<p>If you were not expecting this invitation, you may ignore this email.</p>
+        <p>If you were not expecting this invitation, you may ignore this email.</p>
 
-<p>Best regards,</p>
-<p>Team RentIQ</p>`
-        const mail= await transporter.sendMail({
+        <p>Best regards,</p>
+        <p>Team RentIQ</p>`
+        /* const mail= await transporter.sendMail({
             from: "RentIQ <rentiqapp@gmail.com",
             to: email,
             subject: "Invite to join Rental Property",
             html: message
         })
-        console.log(`mail sent to ${email}`);
+        console.log(`mail sent to ${email}`); */
 
+        const response = await fetch('https://api.brevo.com/v3/smtp/email', {
+            method: 'POST',
+            headers: {
+                'api-key': process.env.brevo_api_key,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                sender: {
+                    name: 'RentIQ',
+                    email: 'rentiqapp@gmail.com' // your verified sender email in Brevo
+                },
+                to: [{ email: email }],
+                subject: "Invite to join Rental Property",
+                htmlContent: message
+            })
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.error('Brevo error:', data);
+            throw new Error(data.message || 'Email sending failed');
+        }
+
+        console.log(`mail sent to ${email}`);
     } catch (error) {
         console.log(error);
     }

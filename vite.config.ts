e7fe935dcba55406033c745c5df0175d24940649
Mayank <mayank@Mayanks-MacBook-Air.local>
2026-03-24 08:53:47 +0530

    fix: force copy _redirects to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.'
+        }
+      ]
+    })
   ],
-  build:{
-    copyPublicDir: true  
-  }
 })
